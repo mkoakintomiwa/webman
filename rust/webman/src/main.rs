1d0dcb65975c0f8811feb2e19583ce6c5a6429cd
mkoakintomiwa <ibnakintomiwa@gmail.com>
2021-09-28 09:40:53 +0100

    Auto Commit

--- rust/webman/src/main.rs
+++ rust/webman/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     let current_exe = std::env::current_exe().unwrap();
     let document_root = current_exe.parent().unwrap().parent().unwrap().to_str().unwrap();
 
-    let script = std::env::args().nth(1).unwrap_or(" nodes --run ");
+    let script = std::env::args().nth(1).unwrap_or("nodes --run ".to_string());
     let argc = std::env::args().len();
     let mut command = format!("{} {}",format!("{}/node/node.exe",document_root),format!("{}/scripts/{}",document_root,script));
 
@@ -12,8 +12,6 @@ fn main() {
             let arg = std::env::args().nth(i).unwrap();
             command = format!("{} {} ",command,arg);   
         }
-    }else{
-        command = format!("{} {} ",command,script);
     }
 
     Exec::shell(command).join().unwrap();
